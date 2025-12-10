c4395b6709adc068ed697a9b5cb69e5d657f2ab1
Kalva Karthik <kalva.karthik@hcltech.com>
2025-12-10 14:23:54 +0530

    passed all test cases

--- 8b_71_10ca_97bf_42ef_b5d6_a96f4b83610e_master/src/main/java/com/hackerrank/gevents/repository/EventRepository.java
+++ 8b_71_10ca_97bf_42ef_b5d6_a96f4b83610e_master/src/main/java/com/hackerrank/gevents/repository/EventRepository.java
@@ -4,6 +4,10 @@ import com.hackerrank.gevents.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
+   List<Event> findAllByRepoIdOrderByIdAsc(Integer repoId);
 }
