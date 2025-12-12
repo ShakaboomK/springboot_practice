a5ac23fc6ebbcdfc05e83507963967ebcaecfacf
Kalva Karthik <kalva.karthik@hcltech.com>
2025-12-12 10:13:45 +0530

    passed SpringBootConfigurationStyles test cases

--- 72_95_f306_16fc_4c4d_9da6_149d855822de_master/src/main/java/com/hackerrank/configstyles/service/EmailNotificationService.java
+++ 72_95_f306_16fc_4c4d_9da6_149d855822de_master/src/main/java/com/hackerrank/configstyles/service/EmailNotificationService.java
@@ -1,10 +1,14 @@
 package com.hackerrank.configstyles.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service("emailNotificationService")
 public class EmailNotificationService implements NotificationService {
+
     private String serviceName;
 
-    public EmailNotificationService(String serviceName) {
+    public EmailNotificationService(@Value("EMAIL_SERVICE") String serviceName) {
         this.serviceName = serviceName;
     }
 
