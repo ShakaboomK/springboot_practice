ca9a93fd54484c9cec962b6ea17de3fde7a3d20d
Kalva Karthik <kalva.karthik@hcltech.com>
2026-01-02 18:22:23 +0530

    to be continued

--- de_83_09e6_7a52_441c_bcf1_d3394463adfc_master/src/main/java/com/hackerrank/stocktrades/controller/StockTradeRestController.java
+++ de_83_09e6_7a52_441c_bcf1_d3394463adfc_master/src/main/java/com/hackerrank/stocktrades/controller/StockTradeRestController.java
@@ -1,7 +1,25 @@
 package com.hackerrank.stocktrades.controller;
 
+import com.hackerrank.stocktrades.model.StockTrade;
+import com.hackerrank.stocktrades.repository.StockTradeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/trades")
 public class StockTradeRestController {
+    private final StockTradeRepository repository;
+
+    public StockTradeRestController(StockTradeRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public ResponseEntity<StockTrade> createStockTrade(@RequestBody StockTrade request){
+        return new ResponseEntity<>(repository.save(request), HttpStatus.CREATED);
+    }
 }
